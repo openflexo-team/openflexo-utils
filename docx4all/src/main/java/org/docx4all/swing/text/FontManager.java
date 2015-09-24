7fa1289cb79105da1b277d5b0ba25ec5020ee948
Sylvain Guérin <sylvain@openflexo.org>
2015-09-24 15:45:18 +0200

    LOW / Attempt to fix font issue when working on server

--- docx4all/src/main/java/org/docx4all/swing/text/FontManager.java
+++ docx4all/src/main/java/org/docx4all/swing/text/FontManager.java
@@ -20,7 +20,6 @@
 package org.docx4all.swing.text;
 
 import java.awt.Font;
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Hashtable;
@@ -43,226 +42,208 @@ import org.jdesktop.application.ResourceMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
- *	@author Jojada Tirtowidjojo - 05/03/2008
+ * @author Jojada Tirtowidjojo - 05/03/2008
  */
 public class FontManager {
 	private static Logger log = LoggerFactory.getLogger(FontManager.class);
-	
+
 	public final static String UNKNOWN_FONT_NAME = "<Not Known>";
 	public final static String UNKNOWN_FONT_SIZE = "##";
-	
+
 	private final static String DOCX4ALL_DEFAULT_FONT_FAMILY_NAME;
 	private final static String DOCX4ALL_DEFAULT_FONT_SIZE;
-	
+
 	private final static FontManager _instance = new FontManager();
-	
+
 	private final static BestMatchingMapper mapper;
-	
-	private final static String[] AVAILABLE_FONT_SIZES = new String[] {
-		UNKNOWN_FONT_SIZE,
-	    "8", "9", "10", "11", "12", "13", "14", "16", "18",
-	    "20", "22", "24", "26", "28", "32", "36", "40", 
-	    "44", "48", "52", "56", "64", "72"
-	};
+
+	private final static String[] AVAILABLE_FONT_SIZES = new String[] { UNKNOWN_FONT_SIZE, "8", "9", "10", "11", "12", "13", "14", "16",
+			"18", "20", "22", "24", "26", "28", "32", "36", "40", "44", "48", "52", "56", "64", "72" };
 
 	private final static String[] AVAILABLE_FONT_FAMILY_NAMES;
-	
+
 	static {
-        log.info("Static initializer..");
-		//Prepare available fonts that are listed in font combobox.
-        log.info("Initialising fonts nameList.");
-		Map<String, MicrosoftFonts.Font> msFontsFilenames = 
-			BestMatchingMapper.getMsFontsFilenames();
+		log.info("Static initializer..");
+		// Prepare available fonts that are listed in font combobox.
+		log.info("Initialising fonts nameList.");
+		Map<String, MicrosoftFonts.Font> msFontsFilenames = BestMatchingMapper.getMsFontsFilenames();
 		List<String> nameList = new ArrayList<String>();
-		for (Map.Entry<String, MicrosoftFonts.Font> entry: msFontsFilenames.entrySet()) {
+		for (Map.Entry<String, MicrosoftFonts.Font> entry : msFontsFilenames.entrySet()) {
 			MicrosoftFonts.Font font = entry.getValue();
-			if (booleanValue(font.isCoreWebFont())
-					|| booleanValue(font.isClearTypeCollection())
-					|| booleanValue(font.isSecondary())) {
+			if (booleanValue(font.isCoreWebFont()) || booleanValue(font.isClearTypeCollection()) || booleanValue(font.isSecondary())) {
 				nameList.add(font.getName());
 			}
 		}
-		
-		//Initialise DOCX4ALL_DEFAULT_FONT.
-		//The font name and size are configured in WordMLEditor.properties file
-        WordMLEditor editor = WordMLEditor.getInstance(WordMLEditor.class);
-        ResourceMap rm = editor.getContext().getResourceMap(WordMLEditor.class);
-        String temp = rm.getString(Constants.APP_DEFAULT_FONT_FAMILY_NAME);
-        if (temp == null || temp.trim().length() == 0) {
-        	temp = "Times New Roman";
-        } else {
-        	temp = temp.trim();
-        }
-        DOCX4ALL_DEFAULT_FONT_FAMILY_NAME = temp;
-        if (!nameList.contains(DOCX4ALL_DEFAULT_FONT_FAMILY_NAME)) {
-			//defaultFontName has to be listed in nameList
-			throw new RuntimeException(
-					"Invalid "
-					+ Constants.APP_DEFAULT_FONT_FAMILY_NAME
-					+ " property value.");
-        }
-        
-        temp = rm.getString(Constants.APP_DEFAULT_FONT_SIZE);
-        if (temp == null || temp.trim().length() == 0) {
-        	temp = "22";
-        } else {
-        	temp = temp.trim();
-        }
-        DOCX4ALL_DEFAULT_FONT_SIZE = temp;
-        boolean invalidSize = true;
-        for (String s: AVAILABLE_FONT_SIZES) {
-        	if (s.equals(DOCX4ALL_DEFAULT_FONT_SIZE)) {
-        		invalidSize = false;
-        	}
-        }
 
-        if (invalidSize) {
-			throw new RuntimeException(
-					"Invalid "
-					+ Constants.APP_DEFAULT_FONT_SIZE
-					+ " property value.");
-        }
-        		
+		// Initialise DOCX4ALL_DEFAULT_FONT.
+		// The font name and size are configured in WordMLEditor.properties file
+		WordMLEditor editor = WordMLEditor.getInstance(WordMLEditor.class);
+		ResourceMap rm = editor.getContext().getResourceMap(WordMLEditor.class);
+		String temp = rm.getString(Constants.APP_DEFAULT_FONT_FAMILY_NAME);
+		if (temp == null || temp.trim().length() == 0) {
+			temp = "Times New Roman";
+		}
+		else {
+			temp = temp.trim();
+		}
+		DOCX4ALL_DEFAULT_FONT_FAMILY_NAME = temp;
+		if (!nameList.contains(DOCX4ALL_DEFAULT_FONT_FAMILY_NAME)) {
+			// defaultFontName has to be listed in nameList
+			throw new RuntimeException("Invalid " + Constants.APP_DEFAULT_FONT_FAMILY_NAME + " property value.");
+		}
+
+		temp = rm.getString(Constants.APP_DEFAULT_FONT_SIZE);
+		if (temp == null || temp.trim().length() == 0) {
+			temp = "22";
+		}
+		else {
+			temp = temp.trim();
+		}
+		DOCX4ALL_DEFAULT_FONT_SIZE = temp;
+		boolean invalidSize = true;
+		for (String s : AVAILABLE_FONT_SIZES) {
+			if (s.equals(DOCX4ALL_DEFAULT_FONT_SIZE)) {
+				invalidSize = false;
+			}
+		}
+
+		if (invalidSize) {
+			throw new RuntimeException("Invalid " + Constants.APP_DEFAULT_FONT_SIZE + " property value.");
+		}
+
 		// Initialise substituter with all available font family names
-        log.info("Initialising substituter.");
+		log.info("Initialising substituter.");
 		mapper = new BestMatchingMapper();
 
-		Set<String> fontsInUse = 
-				new java.util.HashSet<String>(nameList.size());
+		Set<String> fontsInUse = new java.util.HashSet<String>(nameList.size());
 		for (String s : nameList) {
 			fontsInUse.add(s);
 		}
 
 		try {
-			FontTablePart fontTablePart = 
-				new org.docx4j.openpackaging.parts.WordprocessingML.FontTablePart();
+			FontTablePart fontTablePart = new org.docx4j.openpackaging.parts.WordprocessingML.FontTablePart();
 			// For present purposes, use a fairly complete list of
 			// panose values which can be found in the default part.
-			org.docx4j.wml.Fonts tablePartDefaultFonts = 
-				(org.docx4j.wml.Fonts) fontTablePart.unmarshalDefaultFonts();
-			
+			org.docx4j.wml.Fonts tablePartDefaultFonts = (org.docx4j.wml.Fonts) fontTablePart.unmarshalDefaultFonts();
+
 			// NB - we don't actually attach this part to any WordMLPackage
-			
+
 			// Process embedded fonts in fontTablePart.
 			// This has to be done before calling populateFontMappings()
 			// so that the embedded fonts can be taken into account.
 			fontTablePart.processEmbeddings();
-			
+
 			// NB, the above won't have any effect, since there are no
 			// embedded fonts in the default part which we just unmarshalled
-			
+
 			mapper.populateFontMappings(fontsInUse, tablePartDefaultFonts);
 		} catch (Exception exc) {
 			throw new RuntimeException(exc);
 		}
-		
-		
+
 		// Populate AVAILABLE_FONT_FAMILY_NAMES.
 		Collections.sort(nameList);
 		nameList.add(0, UNKNOWN_FONT_NAME);
 		AVAILABLE_FONT_FAMILY_NAMES = new String[nameList.size()];
-		nameList.toArray(AVAILABLE_FONT_FAMILY_NAMES);		
-		
+		nameList.toArray(AVAILABLE_FONT_FAMILY_NAMES);
+
 		log.info("FontManager static initialization complete.");
 	}
-	
-    private final Hashtable<FontTableKey, Font> _fontTable = 
-    	new Hashtable<FontTableKey, Font>();
-    private final FontTableKey _fontTableKey = new FontTableKey(null, 0, 0);
-    
+
+	private final Hashtable<FontTableKey, Font> _fontTable = new Hashtable<FontTableKey, Font>();
+	private final FontTableKey _fontTableKey = new FontTableKey(null, 0, 0);
+
 	public final static FontManager getInstance() {
 		return _instance;
 	}
-	
+
 	private final static boolean booleanValue(Boolean b) {
 		return (b == null) ? false : b.booleanValue();
 	}
-	
+
 	private FontManager() {
 		super();
 	}
-	
+
 	public String[] getAvailableFontFamilyNames() {
 		return AVAILABLE_FONT_FAMILY_NAMES;
 	}
-	
+
 	public String[] getAvailableFontSizes() {
 		return AVAILABLE_FONT_SIZES;
 	}
-	
+
 	public String getDocx4AllDefaultFontFamilyName() {
 		return DOCX4ALL_DEFAULT_FONT_FAMILY_NAME;
 	}
-	
+
 	public int getDocx4AllDefaultFontSize() {
 		return Integer.parseInt(DOCX4ALL_DEFAULT_FONT_SIZE);
 	}
-	
+
 	public String getSourceViewFontFamilyName() {
-        WordMLEditor editor = WordMLEditor.getInstance(WordMLEditor.class);
-        ResourceMap rm = editor.getContext().getResourceMap(WordMLEditor.class);
-        String name = rm.getString(Constants.SOURCE_VIEW_FONT_FAMILY_NAME);
-        if (name == null || name.trim().length() == 0) {
-        	name = getDocx4AllDefaultFontFamilyName();
-        }
-        return name.trim();
+		WordMLEditor editor = WordMLEditor.getInstance(WordMLEditor.class);
+		ResourceMap rm = editor.getContext().getResourceMap(WordMLEditor.class);
+		String name = rm.getString(Constants.SOURCE_VIEW_FONT_FAMILY_NAME);
+		if (name == null || name.trim().length() == 0) {
+			name = getDocx4AllDefaultFontFamilyName();
+		}
+		return name.trim();
 	}
-	
+
 	public int getSourceViewFontSize() {
-        WordMLEditor editor = WordMLEditor.getInstance(WordMLEditor.class);
-        ResourceMap rm = editor.getContext().getResourceMap(WordMLEditor.class);
-        
-        int theSize = getDocx4AllDefaultFontSize();
-        String s = rm.getString(Constants.SOURCE_VIEW_FONT_SIZE);
-        if (s != null && s.length() > 0) {
-        	theSize = Integer.parseInt(s.trim());
-        }
-        return theSize;
+		WordMLEditor editor = WordMLEditor.getInstance(WordMLEditor.class);
+		ResourceMap rm = editor.getContext().getResourceMap(WordMLEditor.class);
+
+		int theSize = getDocx4AllDefaultFontSize();
+		String s = rm.getString(Constants.SOURCE_VIEW_FONT_SIZE);
+		if (s != null && s.length() > 0) {
+			theSize = Integer.parseInt(s.trim());
+		}
+		return theSize;
 	}
-	
+
 	public void addFontsInUse(WordprocessingMLPackage docPackage) {
-		
+
 		// Then is run once when the editor is starting up
 		// from Stylesheet.setWordprocessingMLPackage,
 		// which itself is invoked from:
-		// org.docx4all.swing.text.StyleSheet.getDefaultStyleSheet(StyleSheet.java:68) (on startup)		
+		// org.docx4all.swing.text.StyleSheet.getDefaultStyleSheet(StyleSheet.java:68) (on startup)
 		// and then again when a document is opened.
-		
-		
+
 		log.info("");
-		
+
 		Set fontsInUse = docPackage.getMainDocumentPart().fontsInUse();
 		FontTablePart fontTablePart = docPackage.getMainDocumentPart().getFontTablePart();
-		
+
 		try {
 			// Handle fonts - this is platform specific
 			// Algorithm - to be implemented:
 			// 1. Get a list of all the fonts in the document
 			org.docx4j.wml.Fonts fonts;
 			if (fontTablePart != null) {
-				fonts = (org.docx4j.wml.Fonts) fontTablePart.getJaxbElement();
-			} else {
+				fonts = fontTablePart.getJaxbElement();
+			}
+			else {
 				log.warn("FontTable missing; creating default part.");
-								
+
 				fontTablePart = new org.docx4j.openpackaging.parts.WordprocessingML.FontTablePart();
 				fonts = (org.docx4j.wml.Fonts) fontTablePart.unmarshalDefaultFonts();
 			}
 
-			//2. Process embedded fonts in fontTablePart.
-			//This has to be done before calling populateFontMappings()
-			//so that the embedded fonts can be taken into account.
+			// 2. Process embedded fonts in fontTablePart.
+			// This has to be done before calling populateFontMappings()
+			// so that the embedded fonts can be taken into account.
 			fontTablePart.processEmbeddings();
-			
-			//3. For each font, find the closest match on the system 
+
+			// 3. For each font, find the closest match on the system
 			mapper.populateFontMappings(fontsInUse, fonts);
-			
+
 			if (log.isDebugEnabled()) {
 				int i = 0;
 				Map fontMappings = mapper.getFontMappings();
-				Iterator fontMappingsIterator = fontMappings.entrySet()
-						.iterator();
+				Iterator fontMappingsIterator = fontMappings.entrySet().iterator();
 				while (fontMappingsIterator.hasNext()) {
 					Map.Entry pairs = (Map.Entry) fontMappingsIterator.next();
 
@@ -270,18 +251,18 @@ public class FontManager {
 					PhysicalFont pf = (PhysicalFont) pairs.getValue();
 
 					log.debug("FontMapping[" + (i++) + "]: key=" + key
-//							+ " tripletName=" + fm.getPostScriptName() + " -->> "
-//							+ fm.getEmbeddedFile());
-							+ " -->> "+ pf.getEmbeddedFile());
+					// + " tripletName=" + fm.getPostScriptName() + " -->> "
+					// + fm.getEmbeddedFile());
+							+ " -->> " + pf.getEmbeddedFile());
 				}
 			}
-			
+
 		} catch (Exception exc) {
 			throw new RuntimeException(exc);
 		}
 	}
-	
-    public Font getFontInAction(AttributeSet attr) {
+
+	public Font getFontInAction(AttributeSet attr) {
 		int style = Font.PLAIN;
 		if (StyleConstants.isBold(attr)) {
 			style |= Font.BOLD;
@@ -290,56 +271,54 @@ public class FontManager {
 			style |= Font.ITALIC;
 		}
 		String family = StyleConstants.getFontFamily(attr);
-		
-		//font size in OpenXML is in half points, not points,
-		//so we need to divide by 2
-		int size = StyleConstants.getFontSize(attr)/2;
-		
-		//But Java2D 'point size' appears to be smaller than Windows 'point size'
-		//Adjust with experimental multiplication factor for now.
+
+		// font size in OpenXML is in half points, not points,
+		// so we need to divide by 2
+		int size = StyleConstants.getFontSize(attr) / 2;
+
+		// But Java2D 'point size' appears to be smaller than Windows 'point size'
+		// Adjust with experimental multiplication factor for now.
 		size = size * 14 / 9;
-			
-		//Reduce the font size by 2 for superscript or subscript		
-		if (StyleConstants.isSuperscript(attr)
-				|| StyleConstants.isSubscript(attr)) {
+
+		// Reduce the font size by 2 for superscript or subscript
+		if (StyleConstants.isSuperscript(attr) || StyleConstants.isSubscript(attr)) {
 			size -= 2;
 		}
 
 		return getFontInAction(family, style, size);
 	}
-    
-    private boolean defaultTried = false;
-    
+
+	private boolean defaultTried = false;
+
 	public Font getFontInAction(String fontname, int style, int size) {
 		_fontTableKey.setValue(fontname, style, size);
 		Font theFont = _fontTable.get(_fontTableKey);
-		
+
 		if (theFont == null) {
-			//Not in cache.
-			//Derive from Substituter.FontMapping
-//			String fmKey = SubstituterImplPanose.normalise(family);
+			// Not in cache.
+			// Derive from Substituter.FontMapping
+			// String fmKey = SubstituterImplPanose.normalise(family);
 			PhysicalFont pf = mapper.get(fontname);
 			String path = null;
-			if (pf != null  
-					&& pf.getEmbeddedFile() != null) {
+			if (pf != null && pf.getEmbeddedFile() != null) {
 				path = pf.getEmbeddedFile();
-				
+
 				// Strip file:, and replace %20 with spaces
 				path = org.docx4j.fonts.FontUtils.pathFromURL(path);
-				
+
 				if (log.isDebugEnabled()) {
-					log.debug("family=" + fontname 
-//							+ " fmKey=" + fmKey 
-//							+ " --> FontMapping=" + fm
+					log.debug("family=" + fontname
+					// + " fmKey=" + fmKey
+					// + " --> FontMapping=" + fm
 							+ " - " + path);
 				}
-				
+
 				try {
 					int fontFormat = Font.TRUETYPE_FONT;
 					if (path.toLowerCase().endsWith(".otf") || path.toLowerCase().endsWith(".pfb")) {
 						fontFormat = Font.TYPE1_FONT;
 					}
-					
+
 					/* per http://elliotth.blogspot.com.au/2007/04/far-east-asian-fonts-with-java-7-on.html
 					 * 
 					 * If you call StyleContext.getDefaultStyleContext.getFont instead of new Font, you'll 
@@ -364,75 +343,79 @@ public class FontManager {
 					}
 					
 					// the old way, but recent javac give error (Eclipse is ok) 
-			        //    if (! sun.font.FontManager.fontSupportsDefaultEncoding(theFont)) {
-			        //    	theFont = sun.font.FontManager.getCompositeFontUIResource(theFont);
-			        //    }
-		            //
-		            */
-					
-		            FontTableKey key = new FontTableKey(fontname, style, size);
+					//    if (! sun.font.FontManager.fontSupportsDefaultEncoding(theFont)) {
+					//    	theFont = sun.font.FontManager.getCompositeFontUIResource(theFont);
+					//    }
+					//
+					*/
+
+					FontTableKey key = new FontTableKey(fontname, style, size);
 					_fontTable.put(key, theFont);
 
 				} catch (Exception exc) {
 					// should not happen.
 					throw new RuntimeException(exc);
 				}
-			} else {
+			}
+			else {
 				log.warn("Cannot create font '" + fontname + "'");
-				if (pf==null) {
+				if (pf == null) {
 					log.warn(".. no mapping for that key.");
-				} else {
-					log.warn(".. found a mapping, but getEmbeddedFile returned null!");					
 				}
-					
+				else {
+					log.warn(".. found a mapping, but getEmbeddedFile returned null!");
+				}
+
 				if (defaultTried) {
-					log.info("Failed to get default font; using first available");	
-					return _fontTable.values().iterator().next();
-				} else {
+					if (_fontTable.values().size() > 0) {
+						log.info("Failed to get default font; using first available");
+						return _fontTable.values().iterator().next();
+					}
+					return Font.getFont(fontname);
+				}
+				else {
 					log.info("Using Docx4all default font.");
 					defaultTried = true;
 					theFont = getFontInAction(getDocx4AllDefaultFontFamilyName(), Font.PLAIN, getDocx4AllDefaultFontSize());
 				}
 			}
 		}
-		
+
 		return theFont;
 	}
-		
-    /**
-     * key for TableFont
-     */
-    private static class FontTableKey {
-        private String fontFamilyName;
-        private int fontStyle;
-        private int fontSize;
 
-        public FontTableKey(String fontFamilyName, int fontStyle, int fontSize) {
-            setValue(fontFamilyName, fontStyle, fontSize);
-        }
+	/**
+	 * key for TableFont
+	 */
+	private static class FontTableKey {
+		private String fontFamilyName;
+		private int fontStyle;
+		private int fontSize;
 
-        public void setValue(String fontFamilyName, int fontStyle, int fontSize) {
-            this.fontFamilyName = (fontFamilyName != null) ? fontFamilyName.intern() : null;
-            this.fontStyle = fontStyle;
-            this.fontSize = fontSize;
-        }
-    
-        public int hashCode() {
-    	    int code = (fontFamilyName != null) ? fontFamilyName.hashCode() : 0;
-            return code ^ fontStyle ^ fontSize;
-        }
-        
-        public boolean equals(Object obj) {
-            if (obj instanceof FontTableKey) {
-            	FontTableKey ftk= (FontTableKey) obj;
-                return fontSize == ftk.fontSize 
-                		&& fontStyle == ftk.fontStyle
-                		&& fontFamilyName.equals((ftk.fontFamilyName));
-            }
-            return false;
-        }
-    } //FontTableKey inner class
+		public FontTableKey(String fontFamilyName, int fontStyle, int fontSize) {
+			setValue(fontFamilyName, fontStyle, fontSize);
+		}
 
+		public void setValue(String fontFamilyName, int fontStyle, int fontSize) {
+			this.fontFamilyName = (fontFamilyName != null) ? fontFamilyName.intern() : null;
+			this.fontStyle = fontStyle;
+			this.fontSize = fontSize;
+		}
+
+		@Override
+		public int hashCode() {
+			int code = (fontFamilyName != null) ? fontFamilyName.hashCode() : 0;
+			return code ^ fontStyle ^ fontSize;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (obj instanceof FontTableKey) {
+				FontTableKey ftk = (FontTableKey) obj;
+				return fontSize == ftk.fontSize && fontStyle == ftk.fontStyle && fontFamilyName.equals((ftk.fontFamilyName));
+			}
+			return false;
+		}
+	} // FontTableKey inner class
 
 }// FontManager class
-
