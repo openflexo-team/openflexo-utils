d36cdf652671bae338dba6f29365eb892881de2d
Sylvain Guérin <sylvain@openflexo.org>
2017-03-24 08:54:52 +0100

    MEDIUM / Removed docx4all

--- docx4all/src/main/java/org/plutext/server/transitions/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.plutext.org/transitions", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.plutext.server.transitions;
--- docx4all/src/main/java/org/plutext/transforms/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.plutext.org/transforms", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.plutext.transforms;
