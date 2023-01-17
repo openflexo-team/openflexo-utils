986a18bc9d3bd0c8678303cb37298b99124151f9
Ihab Benamer <benamerihab25@gmail.com>
2023-01-17 15:52:32 +0100

    MEDIUM / update unzip utility

--- flexo-utils/src/main/java/org/openflexo/toolbox/ZipUtils.java
+++ flexo-utils/src/main/java/org/openflexo/toolbox/ZipUtils.java
@@ -39,14 +39,15 @@
 
 package org.openflexo.toolbox;
 
-import java.io.File;
-import java.io.FileFilter;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.ArchiveInputStream;
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
+import org.apache.commons.compress.utils.IOUtils;
+
+import java.io.*;
+import java.nio.file.Files;
 import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class ZipUtils {
@@ -137,36 +138,33 @@ public class ZipUtils {
 		}
 	}
 
-	public static void unzipFile(String source, String target) throws IOException {
-		byte[] buffer 		= new byte[1024];
-		ZipInputStream zis 	= new ZipInputStream(new FileInputStream(source));
-		ZipEntry zipEntry 	= zis.getNextEntry();
 
-		while (zipEntry != null) {
-			File newFile = new File(target, String.valueOf(zipEntry));
-			if (zipEntry.isDirectory()) {
-				if (!newFile.isDirectory() && !newFile.mkdirs()) {
-					throw new IOException("Failed to create directory " + newFile);
-				}
-			} else {
-				// fix for Windows-created archives
-				File parent = newFile.getParentFile();
-				if (!parent.isDirectory() && !parent.mkdirs()) {
-					throw new IOException("Failed to create directory " + parent);
-				}
+	public static void unzipFile(String source, String destDirectory) throws IOException {
+		FileInputStream zipFile = new FileInputStream(source);
 
-				// write file content
-				FileOutputStream fos = new FileOutputStream(newFile);
-				int len;
-				while ((len = zis.read(buffer)) > 0) {
-					fos.write(buffer, 0, len);
+		try (ArchiveInputStream i = new ZipArchiveInputStream(zipFile, "UTF-8", false, true)) {
+			ArchiveEntry entry = null;
+			while ((entry = i.getNextEntry()) != null) {
+				if (!i.canReadEntryData(entry)) {
+					System.out.println("Can't read entry: " + entry);
+					continue;
 				}
-				fos.close();
+				String name = destDirectory + File.separator + entry.getName();
+				File f = new File(name);
+				if (entry.isDirectory()) {
+					if (!f.isDirectory() && !f.mkdirs()) {
+						throw new IOException("failed to create directory " + f);
+					}
+				} else {
+					File parent = f.getParentFile();
+					if (!parent.isDirectory() && !parent.mkdirs()) {
+						throw new IOException("failed to create directory " + parent);
+					}
+					try (OutputStream o = Files.newOutputStream(f.toPath())) {
+						IOUtils.copy(i, o);
+					}
+				}
 			}
-			zipEntry = zis.getNextEntry();
 		}
-		zis.closeEntry();
-		zis.close();
-
 	}
 }
