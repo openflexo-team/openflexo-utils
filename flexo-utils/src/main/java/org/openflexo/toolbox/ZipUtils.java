7b335537e3519744d3f1eb865bede1c9ad21ee13
Ihab Benamer <benamerihab25@gmail.com>
2023-01-25 13:23:07 +0100

    MEDIUM / add unzip utility

--- flexo-utils/src/main/java/org/openflexo/toolbox/ZipUtils.java
+++ flexo-utils/src/main/java/org/openflexo/toolbox/ZipUtils.java
@@ -39,11 +39,13 @@
 
 package org.openflexo.toolbox;
 
-import java.io.File;
-import java.io.FileFilter;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.ArchiveInputStream;
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
+import org.apache.commons.compress.utils.IOUtils;
+
+import java.io.*;
+import java.nio.file.Files;
 import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -132,6 +134,35 @@ public class ZipUtils {
 			// now write the content of the file to the ZipOutputStream
 			while ((bytesIn = fis.read(readBuffer)) != -1) {
 				zos.write(readBuffer, 0, bytesIn);
+			}
+		}
+	}
+
+	public static void unzipFile(String source, String destDirectory) throws IOException {
+		FileInputStream zipFile = new FileInputStream(source);
+
+		try (ArchiveInputStream i = new ZipArchiveInputStream(zipFile, "UTF-8", false, true)) {
+			ArchiveEntry entry = null;
+			while ((entry = i.getNextEntry()) != null) {
+				if (!i.canReadEntryData(entry)) {
+					System.out.println("Can't read entry: " + entry);
+					continue;
+				}
+				String name = destDirectory + File.separator + entry.getName();
+				File f = new File(name);
+				if (entry.isDirectory()) {
+					if (!f.isDirectory() && !f.mkdirs()) {
+						throw new IOException("failed to create directory " + f);
+					}
+				} else {
+					File parent = f.getParentFile();
+					if (!parent.isDirectory() && !parent.mkdirs()) {
+						throw new IOException("failed to create directory " + parent);
+					}
+					try (OutputStream o = Files.newOutputStream(f.toPath())) {
+						IOUtils.copy(i, o);
+					}
+				}
 			}
 		}
 	}
